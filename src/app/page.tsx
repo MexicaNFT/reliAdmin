1dc78206b2a618d8dc38f85bdc8b746120645b3d
Pablo Banzo <pridapablo@gmail.com>
2024-10-30 13:04:41 +0100

    Refactor Authenticator component to hide sign up option

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
   return (
     <main className="container mx-auto p-4">
-      <Authenticator>
+      <Authenticator hideSignUp>
         {({ signOut }) => (
           <>
             <AdminPanel apiName={apiName} />
